ab44068af0b84b98c615ce9f47711ee6d3fd1491
Jack Saysana <saysanabjack@gmail.com>
2022-04-22 14:28:35 -0400

    Implemented Firebase Emulation

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const helmet = require("helmet");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const source = require("./src/app");
@@ -9,6 +10,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(helmet());
+app.use(bodyParser.json());
 app.use('/home', source);
 
 exports.app = functions.https.onRequest(app);
--- functions/src/app.js
+++ functions/src/app.js
@@ -1,3 +1,24 @@
+const { initializeApp } = require("firebase/app");
+const { getFirestore, connectFirestoreEmulator } = require("firebase/firestore");
+const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, connectAuthEmulator } = require("firebase/auth");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtuKD3x-8ggb5UoZsuX4MF-qQhoIMK0_k",
+    authDomain: "pbay-51219.firebaseapp.com",
+    projectId: "pbay-51219",
+    storageBucket: "pbay-51219.appspot.com",
+    messagingSenderId: "825090402653",
+    appId: "1:825090402653:web:33cf280f3e6c071bbe20b4",
+    measurementId: "G-F5FKK2C9M4"
+};
+
+const fb = initializeApp(firebaseConfig);
+const db = getFirestore(fb);
+const auth = getAuth();
+
+connectAuthEmulator(auth, "http://localhost:9099");
+//connectFirestoreEmulator(db, 'localhost', 4000);
+
 const express = require("express");
 const app = express();
 
@@ -125,8 +146,27 @@ const listings = [
     }
 ];
 
+app.post("/signup", (req, res) => {
+    createUserWithEmailAndPassword(auth, req.body.email, req.body.password).then((cred) => {
+        res.status(200).json({ user: cred.user });
+    }).catch((err) => {
+        res.status(400).json({ code: err.code, msg: err.message });
+    });
+});
+
+app.post("/login", (req, res) => {
+    signInWithEmailAndPassword(auth, req.body.email, req.body.password).then((cred) => {
+        res.status(200).json({ user: cred.user });
+    }).catch((err) => {
+        res.status(400).json({ code: err.code, msg: err.message });
+    });
+});
+
+// app.post("/item", (req, res) => {
+
+// });
+
 app.get("/", (req, res) => {
-    console.log("Home");
     res.render("pages/index", { listings: listings });
 });
 
