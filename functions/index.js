07787816c954b83e6d2a1940404797da717ade3b
Zack Bostock <zbostock56@gmail.com>
2022-04-25 12:36:52 -0400

    Added Content Security Policy

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        "script-src": ['self', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"]
+    }
+}));
 app.use('/', source);
 
 exports.app = functions.https.onRequest(app);
