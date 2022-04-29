ceb536bcd37f9f405b5807fd767edad213f5ff4e
Zack Bostock <zbostock56@gmail.com>
2022-04-28 21:41:48 -0400

    Added new category pages and major updates to pages

--- functions/shell_scripts/run_mac.sh
+++ functions/shell_scripts/run_mac.sh
@@ -1,14 +1,17 @@
 lsof -i tcp:3000 | grep "^[0-9][0-9][0-9][0-9][0-9]*$"
 if [ -z "$(lsof -i tcp:3000)" ]
 then
-      echo "*********** \$OUTPUT is empty **************"
+      echo "*********** Output IS empty **************"
       cd ../functions/
       pwd
       npm run devStart
 else
-      echo "*********** \$OUTPUT is NOT empty ***********"
-      echo "The output was:\n$OUTPUT"
+      echo "!!!!!!!!!!!!! Output is NOT empty !!!!!!!!!!"
+      echo "The output was:"
+      lsof -i tcp:3000
       echo "Killing the processes..."
+      echo "--------------------------------------------"
+      echo ""
       kill -9 $(lsof -i tcp:3000) | $(grep "^[0-9][0-9][0-9][0-9][0-9]*$")
       cd ../functions/
       pwd
